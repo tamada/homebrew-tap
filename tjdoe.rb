f018b110e7ac53423a39bb54afd05815c18a586a
Haruaki Tamada <tamada@users.noreply.github.com>
2019-12-26 17:48:03 +0900

    fix typo.

--- tjdoe.rb
+++ tjdoe.rb
@@ -5,7 +5,7 @@ HOMEBREW_TJDOE_VERSION="1.0.0"
 class Tjdoe < Formula
   desc "anonymizing the programs of the assignments in the programming courses and their score for grades."
   homepage "https://github.com/tamada/djdoe"
-  url "https://github.com/tamada/tjdoe/releases/download/v1.0.0/tjdoe-#{HOMEBREW_TTT_VERSION}_darwin_amd64.tar.gz"
+  url "https://github.com/tamada/tjdoe/releases/download/v1.0.0/tjdoe-#{HOMEBREW_TJDOE_VERSION}_darwin_amd64.tar.gz"
   version HOMEBREW_TJDOE_VERSION
   sha256 "563c8d4349f1b1503c85b0c8489671f3e6639064449625401bb53124dcf02a9b"
 
