8f6117ebd0731d5ccb71783f25a06e52eae1cc44
Haruaki Tamada <tamada@users.noreply.github.com>
2019-12-26 17:51:32 +0900

    update sha256

--- tjdoe.rb
+++ tjdoe.rb
@@ -7,7 +7,7 @@ class Tjdoe < Formula
   homepage "https://github.com/tamada/djdoe"
   url "https://github.com/tamada/tjdoe/releases/download/v1.0.0/tjdoe-#{HOMEBREW_TJDOE_VERSION}_darwin_amd64.tar.gz"
   version HOMEBREW_TJDOE_VERSION
-  sha256 "563c8d4349f1b1503c85b0c8489671f3e6639064449625401bb53124dcf02a9b"
+  sha256 "e58f4634fb35ea351cca0bf5ac08c4a80474dea956182c2e67627c3924239381"
 
   depends_on "go"  => :build
 
