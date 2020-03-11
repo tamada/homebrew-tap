ead24ee13c3cee2e7c5fa25454b2a6a2d37f403a
Haruaki Tamada <tamada@users.noreply.github.com>
2020-03-11 23:20:11 +0900

    fixed invalid url.

--- uniq2.rb
+++ uniq2.rb
@@ -5,7 +5,7 @@ HOMEBREW_UNIQ2_VERSION="1.1.1"
 class Uniq2 < Formula
   desc "Eliminating duplicated lines"
   homepage "https://github.com/tamada/uniq2"
-  url "https://github.com/tamada/uniq2/releases/download/v#{HOMEBREW_UNIQ2_VERSION}/uniq2-v#{HOMEBREW_UNIQ2_VERSION}_darwin_amd64.tar.gz"
+  url "https://github.com/tamada/uniq2/releases/download/v#{HOMEBREW_UNIQ2_VERSION}/uniq2-#{HOMEBREW_UNIQ2_VERSION}_darwin_amd64.tar.gz"
   version HOMEBREW_UNIQ2_VERSION
   sha256 "98facbeb8e134609eccead48031e5e411e1e723b9f8b9f48b9758ce3809c0eff"
 
