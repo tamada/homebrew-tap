62cae3b8b3145e3218b3f958a1920922372e5d67
Haruaki Tamada <tamada@users.noreply.github.com>
2020-03-11 23:02:17 +0900

    update uniq2 version 1.1.1

--- uniq2.rb
+++ uniq2.rb
@@ -1,13 +1,13 @@
 require "formula"
 
-HOMEBREW_UNIQ2_VERSION="1.0.2"
+HOMEBREW_UNIQ2_VERSION="1.1.1"
 
 class Uniq2 < Formula
   desc "Eliminating duplicated lines"
   homepage "https://github.com/tamada/uniq2"
   url "https://github.com/tamada/uniq2/releases/download/v#{HOMEBREW_UNIQ2_VERSION}/uniq2-v#{HOMEBREW_UNIQ2_VERSION}_darwin_amd64.tar.gz"
   version HOMEBREW_UNIQ2_VERSION
-  sha256 "99c64c4f68b4219d4cb36ff1e63bcd84ad2a94aee921f2852a4cb2130d597ee7"
+  sha256 "98facbeb8e134609eccead48031e5e411e1e723b9f8b9f48b9758ce3809c0eff"
 
   def install
     bin.install "uniq2"
