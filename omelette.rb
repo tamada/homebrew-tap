7c52e686201b59e3fe89c295263da1faf9ff7918
Haruaki Tamada <tamada@users.noreply.github.com>
2020-07-17 11:15:15 +0900

    update omelette version 1.1.1

--- omelette.rb
+++ omelette.rb
@@ -1,13 +1,13 @@
 require "formula"
 
-HOMEBREW_OMELETTE_VERSION="1.1.0"
+HOMEBREW_OMELETTE_VERSION="1.1.1"
 
 class Omelette < Formula
   desc "An agent for running unit tests/calculating coverages on the CLI environemnt for the Java platform."
   homepage "https://github.com/tamada/omelette"
   url "https://github.com/tamada/omelette/releases/download/v#{HOMEBREW_OMELETTE_VERSION}/omelette-#{HOMEBREW_OMELETTE_VERSION}_darwin_amd64.tar.gz"
   version HOMEBREW_OMELETTE_VERSION
-  sha256 "5b849c3b2f621da6520326aaf079b21de086ca4dc9a7676a6d4593a45089afcb"
+  sha256 "a2c0f51c13fc46686a6ae6ae1ceaf02a1b8b37c547a3f6cf91922bc7c9b52a65"
 
   depends_on "bash_completion@2" => :optional
   depends_on "java" => :optional
