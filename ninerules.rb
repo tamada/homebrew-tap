9a9afb2fc7c9c3def225ef8a1f94c9349c19de6a
Haruaki Tamada <tamada@users.noreply.github.com>
2020-09-15 11:36:58 +0900

    update 9rules

--- ninerules.rb
+++ ninerules.rb
@@ -4,14 +4,14 @@ require "formula"
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
-HOMEBREW_9RULES_VERSION="1.0.0"
+HOMEBREW_9RULES_VERSION="1.1.0"
 
 class Ninerules < Formula
   desc "Checking tool of small object programming for the Java language"
   homepage "https://github.com/tamada/9rules"
   url "https://github.com/tamada/9rules/releases/download/v#{HOMEBREW_9RULES_VERSION}/9rules-#{HOMEBREW_9RULES_VERSION}-bin.zip"
   version HOMEBREW_9RULES_VERSION
-  sha256 "08922a8c569a362b7e693b07cf614b6d8ab67d6e4dd9d3142991f6e41a621fee"
+  sha256 "c4e27c371ff2f52e51998752b29db6a061dc6e813306d66547b67822cf05ba34"
 
   def install
     (bin/"9rules").write <<~EOS
