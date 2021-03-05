4e8966fccb22d5d37420d0646565cbf839988657
Haruaki Tamada <tamada@users.noreply.github.com>
2021-03-05 23:30:20 +0900

    ship wildcat v1.0.3

--- wildcat.rb
+++ wildcat.rb
@@ -1,13 +1,13 @@
 require "formula"
 
-HOMEBREW_WILDCAT_VERSION="1.0.2"
+HOMEBREW_WILDCAT_VERSION="1.0.3"
 
 class Wildcat < Formula
   desc "Another implementation of wc (word count)"
   homepage "https://github.com/tamada/wildcat"
   url "https://github.com/tamada/wildcat/releases/download/v#{HOMEBREW_WILDCAT_VERSION}/wildcat-#{HOMEBREW_WILDCAT_VERSION}_darwin_amd64.tar.gz"
   version HOMEBREW_WILDCAT_VERSION
-  sha256 "d7122db26553f0175a0885fc8f0888f603f35f2c9955e7c4226b5b6e3864bf0c"
+  sha256 "i290aebc46a85ad075b4429d699ae12754b5d4a56211b01509d413478a7b41ed7"
 
   depends_on "bash_completion@2" => :optional
 
