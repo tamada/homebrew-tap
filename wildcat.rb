68d75069b81531d1a086282a28abe6d2b449f599
Haruaki Tamada <tamada@users.noreply.github.com>
2021-04-28 15:47:28 +0900

    ship wildcat-1.2.0

--- wildcat.rb
+++ wildcat.rb
@@ -1,11 +1,11 @@
-VERSION="1.1.1"
+VERSION="1.2.0"
 
 class Wildcat < Formula
   desc "Another implementation of wc (word count)"
   homepage "https://github.com/tamada/wildcat"
   url "https://github.com/tamada/wildcat/releases/download/v#{VERSION}/wildcat-#{VERSION}_darwin_amd64.tar.gz"
   version VERSION
-  sha256 "a10fff0f9569b1ea4fdb298ed5adb2ac9b79fd3085f518b84fbecc7a07d8fdc8"
+  sha256 "308046086a8531f162b3096002951adb629c55a6c99e7d335f7f60732ff93a67"
   license "Apache-2.0"
 
   option "without-completions", "Disable bash completions"
