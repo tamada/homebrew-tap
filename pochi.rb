29f5792d2ee393b8db6fb602acfd45d994a77b88
Haruaki Tamada <tamada@users.noreply.github.com>
2021-07-29 13:04:00 +0900

    update pochi to 2.4.6

--- pochi.rb
+++ pochi.rb
@@ -1,24 +1,24 @@
-POCHI_VERSION = "2.2.0"
+POCHI_VERSION = "2.4.6"
 
 class Pochi < Formula
   desc "Java birthmark toolkit, detecting the software theft by native characteristics of the programs."
   homepage "https://tamada.github.io/pochi"
   url "https://github.com/tamada/pochi/releases/download/v#{POCHI_VERSION}/pochi-#{POCHI_VERSION}.zip"
-  sha256 "b38a5c33d2df33c8b31777e5e2175aa9a3a056dd780c2f90e3ae139c44a24267"
+  sha256 "0b5556952c3f76f60bbae18171781fecca31198ab948d08889a60505f97c6981"
 
   option "without-completions", "Disable bash completions"
   depends_on "bash-completion@2" => :optional
 
   def install
-    prefix.install "Dockerfile"
     prefix.install "docs"
+    prefix.install "data"
     prefix.install "examples"
     prefix.install "lib"
     prefix.install "LICENSE"
     prefix.install "README.md"
     bin.install "bin/pochi"
 
-    bash_completion.install "completions/bash/pochi.bash" if build.with? "completions"
+    bash_completion.install "completions/bash/pochi" if build.with? "completions"
   end
 
   def caveats
