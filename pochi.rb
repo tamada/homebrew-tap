19b1e8f80c2212fc8086f975df62f3498834f9e7
Haruaki Tamada <tamada@users.noreply.github.com>
2021-09-14 11:16:26 +0900

    update version pochi 2.5.0 to 2.5.1

--- pochi.rb
+++ pochi.rb
@@ -1,10 +1,10 @@
-POCHI_VERSION = "2.5.0"
+POCHI_VERSION = "2.5.1"
 
 class Pochi < Formula
   desc "Java birthmark toolkit, detecting the software theft by native characteristics of the programs."
   homepage "https://tamada.github.io/pochi"
   url "https://github.com/tamada/pochi/releases/download/v#{POCHI_VERSION}/pochi-#{POCHI_VERSION}.zip"
-  sha256 "fb1555bb05bcd041d9255adce76c053c01ef93632558ae8d40a1e4857a8ad041"
+  sha256 "8da357c185458e31bab8d624fe89584bf4b0f9bb4039a1097b4d4190b6cc9b20"
 
   option "without-completions", "Disable bash completions"
   depends_on "bash-completion@2" => :optional
@@ -12,6 +12,7 @@ class Pochi < Formula
   def install
     prefix.install "data"
     prefix.install "docs"
+    prefix.install "dockers"
     prefix.install "examples"
     prefix.install "lib"
     prefix.install "LICENSE"
