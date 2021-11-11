94656e428f99c42a13f326fa5863f6d9d014b812
Haruaki Tamada <tamada@users.noreply.github.com>
2021-11-11 10:46:55 +0900

    publish sibling 1.1.2

--- sibling.rb
+++ sibling.rb
@@ -1,11 +1,11 @@
-HOMEBREW_SIBLING_VERSION="1.1.1"
+HOMEBREW_SIBLING_VERSION="1.1.2"
 
 class Sibling < Formula
   desc "get next/previous sibling directory name."
   homepage "https://github.com/tamada/sibling"
   url "https://github.com/tamada/sibling/releases/download/v#{HOMEBREW_SIBLING_VERSION}/sibling-#{HOMEBREW_SIBLING_VERSION}_darwin_amd64.tar.gz"
   version HOMEBREW_SIBLING_VERSION
-  sha256 "7b08346365bf9232c72433d297e77035fe83dde83c9964c60fb493ed90a353ce"
+  sha256 "d7699f1ed97097c7641539561b7624cdc38fdaf56574f1569e538cf563715ebd"
 
   option "without-completions", "Disable bash completions"
   depends_on "bash-completion@2" => :optional
