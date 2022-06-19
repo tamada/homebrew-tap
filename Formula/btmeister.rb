ce58eca0e789df39144bcf2eaed51e0b6ad20df7
Haruaki Tamada <tamada@users.noreply.github.com>
2022-06-20 00:44:25 +0900

    publish btmeister

--- Formula/btmeister.rb
+++ Formula/btmeister.rb
@@ -1,26 +1,24 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+VERSION = "0.3.21"
+
 class Btmeister < Formula
   desc "Detecting the build tools in use"
   homepage "https://github.com/tamada/btmeister"
-  version "0.1.2"
+  url "https://github.com/tamada/btmeister/releases/download/v#{VERSION}/btmeister-#{VERSION}_darwin_amd64.tar.gz"
+  sha256 "6f1bb83790528477081bbaf68bdffbda9810fa53609fa3ca39bcb661b2ef5f9f"
   license "MIT"
-  head "https://github.com/tamada/btmeister.git", branch: "main"
 
   option "without-completions", "Disable bash completions"
   depends_on "rustup" => :build
   depends_on "bash-completion@2" => :optional
 
   def install
-    system "cargo", "build"
-    bin.install "target/release/btmeister"
+    bin.install "btmeister"
     prefix.install "README.md"
     prefix.install "LICENSE"
-    prefix.install "target/completions"
+    prefix.install "completions"
 
-    bash_completion.install "target/completions/bash/btmeister" if build.with? "completions"
-    zsh_completion.install "target/completions/zsh/_btmeister" if build.with? "completions"
-    fish_completion.install "target/completions/fish/btmeister" if build.with? "completions"
+    bash_completion.install "completions/bash/btmeister" if build.with? "completions"
+    zsh_completion.install  "completions/zsh/_btmeister" if build.with? "completions"
+    fish_completion.install "completions/fish/btmeister" if build.with? "completions"
   end
 end
