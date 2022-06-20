ec863ad51493cbe71026ebdf9f3ed96942ccf148
Haruaki Tamada <tamada@users.noreply.github.com>
2022-06-20 12:27:27 +0900

    btmeister release 0.4.7

--- Formula/btmeister.rb
+++ Formula/btmeister.rb
@@ -1,10 +1,10 @@
-VERSION = "0.3.21"
+VERSION = "0.4.7"
 
 class Btmeister < Formula
   desc "Detecting the build tools in use"
   homepage "https://github.com/tamada/btmeister"
   url "https://github.com/tamada/btmeister/releases/download/v#{VERSION}/btmeister-#{VERSION}_darwin_amd64.tar.gz"
-  sha256 "6f1bb83790528477081bbaf68bdffbda9810fa53609fa3ca39bcb661b2ef5f9f"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
   option "without-completions", "Disable bash completions"
