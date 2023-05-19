000353efaf9797fa3cea4c5fd0627db9ef80b892
Haruaki Tamada <tamada@users.noreply.github.com>
2023-05-20 08:46:46 +0900

    release sibling 1.2.4

--- Formula/sibling.rb
+++ Formula/sibling.rb
@@ -1,11 +1,11 @@
-HOMEBREW_SIBLING_VERSION="1.1.2"
+HOMEBREW_SIBLING_VERSION="1.2.4"
 
 class Sibling < Formula
   desc "get next/previous sibling directory name."
   homepage "https://github.com/tamada/sibling"
-  url "https://github.com/tamada/sibling/releases/download/v#{HOMEBREW_SIBLING_VERSION}/sibling-#{HOMEBREW_SIBLING_VERSION}_darwin_amd64.tar.gz"
+  url "https://github.com/tamada/sibling/releases/download/v#{HOMEBREW_SIBLING_VERSION}/sibling-#{HOMEBREW_SIBLING_VERSION}_darwin_arm64.tar.gz"
   version HOMEBREW_SIBLING_VERSION
-  sha256 "6f13a912f9189474a723d29508302d7d83b8d3bf2346b67ec3058a3ee2327942"
+  sha256 "bab3bab6873a1cc4b97572dff3dd91690cac07fbc2a2a11b405046d746286950"
 
   option "without-completions", "Disable bash completions"
   depends_on "bash-completion@2" => :optional
