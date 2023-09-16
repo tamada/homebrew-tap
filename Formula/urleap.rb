bdb0e43346cd18462d62e2deee11fb41e200f8ac
Haruaki Tamada <tamada@users.noreply.github.com>
2023-09-17 08:10:00 +0900

    update release version of urleap

--- Formula/urleap.rb
+++ Formula/urleap.rb
@@ -1,32 +1,28 @@
-VERSION="0.1.6"
+VERSION="v0.2.5"
 
 class Urleap < Formula
   desc "URL shortener via bit.ly"
-  homepage "https://github.com/tamada/urleap"
-  version VERSION
-  license "MIT"
-
   option "without-completions", "Disable bash completions"
   depends_on "bash-completion@2" => :optional
 
+  homepage "https://github.com/tamada/urleap"
+  version VERSION
+  license "MIT License"
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tamada/urleap/releases/download/v#{VERSION}/urleap-#{VERSION}_darwin_amd64.tar.gz"
-    sha256 "72865931aea7ea1231e8dc100468d54393a4f1c485ab636b9f8a479dc8aa9928"
+    url "https://github.com/tamada/urleap/releases/download/v0.2.5/urleap-0.2.5_darwin_amd64.tar.gz"
+    sha256 "65e736275551ba53627d7f218fae704800e61489af1a11b383dc6624f6d8d793"
   end
-
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tamada/urleap/releases/download/v#{VERSION}/urleap-#{VERSION}_darwin_arm64.tar.gz"
-    sha256 "af98f0d6699203531139815de9223a71d543d14446b1e60f5b77a005eeca7454"
+    url "https://github.com/tamada/urleap/releases/download/v0.2.5/urleap-0.2.5_darwin_arm64.tar.gz"
+    sha256 "ac2b2b76f30bd3fba80855b87ce79789051c7a281ae23679863262901908be9a"
   end
-
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tamada/urleap/releases/download/v#{VERSION}/urleap-#{VERSION}_linux_amd64.tar.gz"
-    sha256 "42f0d6e75dc6215f95d4309895a6eea91f93d4400ab9d06497227dbcf186aa6d"
+    url "https://github.com/tamada/urleap/releases/download/v0.2.5/urleap-0.2.5_linux_amd64.tar.gz"
+    sha256 "c3915f948a519ef1d9885e28306c9e2b5355beb51ac74c403290e5a2a9886a82"
   end
-
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/tamada/urleap/releases/download/v#{VERSION}/urleap-#{VERSION}_linux_arm64.tar.gz"
-    sha256 "b35bed4f8570966d9cefeb673b37ccfd3685a3fe922821143243b9b73671a02c"
+    url "https://github.com/tamada/urleap/releases/download/v0.2.5/urleap-0.2.5_linux_arm64.tar.gz"
+    sha256 "9312ca60133df13d193c56c208730646b12ad97ec0add86863cf02796e9576f1"
   end
 
   def install
