e84e2c0cd3fc4235c699d4ed5be0d586b4e58cb6
Haruaki Tamada <tamada@users.noreply.github.com>
2024-12-23 14:53:12 +0900

    update sibling to version v2.0.0-beta-6 and update release information

--- Formula/sibling.rb
+++ Formula/sibling.rb
@@ -1,4 +1,4 @@
-VERSION="v2.0.0-beta-3"
+VERSION="v2.0.0-beta-6"
 
 class Sibling < Formula
   desc "get next/previous sibling directory name."
@@ -6,20 +6,20 @@ class Sibling < Formula
   version VERSION
   license "WTFPL License"
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tamada/sibling/releases/download/v2.0.0-beta-3/sibling-2.0.0-beta-3_darwin_amd64.tar.gz"
-    sha256 "b2233ffa0642e52c293affee78e1bb7090c433c9ab330a078d58993528d42c73"
+    url "https://github.com/tamada/sibling/releases/download/v2.0.0-beta-6/sibling-2.0.0-beta-6_darwin_amd64.tar.gz"
+    sha256 "cb45de13b5d469de7a45aa524cb9e8da90207e43b29d8b221d20b01cbc869292"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tamada/sibling/releases/download/v2.0.0-beta-3/sibling-2.0.0-beta-3_darwin_arm64.tar.gz"
-    sha256 "295b01b5b1026b3dd9e06412c054b5e22843524f7260292e673b116e9fe2fa8e"
+    url "https://github.com/tamada/sibling/releases/download/v2.0.0-beta-6/sibling-2.0.0-beta-6_darwin_arm64.tar.gz"
+    sha256 "55bde100b7da2e4f0300d8a5043f06367d222f0882fbba70fded9dd73b0fcbf5"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tamada/sibling/releases/download/v2.0.0-beta-3/sibling-2.0.0-beta-3_linux_amd64.tar.gz"
-    sha256 "75fba5964c1438206372f034c8f043f5130f49ed68e8b775b7b2a161ef6c2104"
+    url "https://github.com/tamada/sibling/releases/download/v2.0.0-beta-6/sibling-2.0.0-beta-6_linux_amd64.tar.gz"
+    sha256 "51d2233d86f9e6ba24708d7f8168d708c6c205804cff8c5264d9a27e3f0098b5"
   end
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/tamada/sibling/releases/download/v2.0.0-beta-3/sibling-2.0.0-beta-3_linux_arm64.tar.gz"
-    sha256 "26e33d6bd961997454d3eff8eadc6d0c24fc02cdd695b732c0b5023f81eb0325"
+    url "https://github.com/tamada/sibling/releases/download/v2.0.0-beta-6/sibling-2.0.0-beta-6_linux_arm64.tar.gz"
+    sha256 "ff5fa66402a039eec77684f4578245eee1d9f350aec2ee42aba1c82b410d74e4"
   end
 
 
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Homebrew recipes for my products.
 
 | Products | Url | Formula | Description | Version | Release date |
 |----------|-----|---------|-------------|---------|--------------|
-| sibling | [:octocat:](https://github.com/tamada/sibling)[:spider_web:](https://tamada.github.io/sibling/)| `tamada/tap/sibling` | get next/previous sibling directory name. |v2.0.0-beta-3 | 2024-12-05 |
+| sibling | [:octocat:](https://github.com/tamada/sibling)[:spider_web:](https://tamada.github.io/sibling/)| `tamada/tap/sibling` | get next/previous sibling directory name. |v2.0.0-beta-6 | 2024-12-23 |
 | gibo-wrapper | [:octocat:](https://github.com/tamada/gibo-wrapper)[:spider_web:](https://tamada.github.io/gibo-wrapper/)| `tamada/tap/gibo-wrapper` | gibo-wrapper acts like gibo and improves gibo by adding some features. |v0.9.0 | 2024-11-30 |
 | totebag | [:octocat:](https://github.com/tamada/totebag)[:spider_web:](https://tamada.github.io/totebag/)| `tamada/tap/totebag` | A tool for archiving files and directories and extracting several archive formats. |v0.3.0 | 2024-05-14 |
 | peripherals | [:octocat:](https://github.com/tamada/peripherals)[:spider_web:](https://tamada.github.io/peripherals/)| `tamada/tap/peripherals` | Peripheral Utility Commands for the Shell Environment |v1.0.1 | 2023-08-05 |
