926bd141af7aec957ef8b2b3e275d60eafe50299
Haruaki Tamada <tamada@users.noreply.github.com>
2025-09-11 08:40:18 +0900

    refactor: Simplify asset handling in template files by removing redundant URL and SHA256 definitions

--- .template/urleap_template.rb
+++ .template/urleap_template.rb
@@ -12,28 +12,19 @@ class Urleap < Formula
   {%- for asset in release.assets %}
     {%- if "darwin" in asset.name and "amd64" in asset.name %}
   if OS.mac? && Hardware::CPU.intel?
-    url "{{ asset.url }}"
-    sha256 "{{ asset.url | sha256 }}"
-  end
     {%- endif %}
     {%- if "darwin" in asset.name and "arm64" in asset.name %}
   if OS.mac? && Hardware::CPU.arm?
-    url "{{ asset.url }}"
-    sha256 "{{ asset.url | sha256 }}"
-  end
     {%- endif %}
     {%- if "linux" in asset.name and "amd64" in asset.name %}
   if OS.linux? && Hardware::CPU.intel?
-    url "{{ asset.url }}"
-    sha256 "{{ asset.url | sha256 }}"
-  end
     {%- endif %}
     {%- if "linux" in asset.name and "arm64" in asset.name %}
   if OS.linux? && Hardware::CPU.arm?
+    {%- endif %}
     url "{{ asset.url }}"
     sha256 "{{ asset.url | sha256 }}"
   end
-    {%- endif %}
   {%- endfor %}
 
   def install
