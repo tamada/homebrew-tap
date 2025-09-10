aeae2fd49a501ba4cc3c29490970cacc8261744d
Haruaki Tamada <tamada@users.noreply.github.com>
2025-09-11 08:32:21 +0900

    refactor: Update template files to use consistent release variable naming and streamline asset handling

--- .template/rrh_template.rb
+++ .template/rrh_template.rb
@@ -1,5 +1,4 @@
-{% set r = release %}
-VERSION="{{ r.tagName }}"
+VERSION="{{ release.tagName }}"
 
 class Rrh < Formula
   desc "Repositories, Ready to Hack/Remote Repositories Head/Red Riding Hood"
@@ -7,52 +6,32 @@ class Rrh < Formula
   version VERSION
   license "{{ project.license }}"
 
-  {% set DARWIN_AMD64 = "" %}
-  {% set DARWIN_ARM64 = "" %}
-  {% set LINUX_AMD64 = "" %}
-  {% set LINUX_ARM64 = "" %}
-  {% for asset in r.assets %}
-    {% if "darwin" in asset.name and "amd64" in asset.name %}
-      {% set DARWIN_AMD64 = asset.url %}
-    {% endif %}
-    {% if "darwin" in asset.name and "arm64" in asset.name %}
-      {% set DARWIN_ARM64 = asset.url %}
-    {% endif %}
-    {% if "linux" in asset.name and "amd64" in asset.name %}
-      {% set LINUX_AMD64 = asset.url %}
-    {% endif %}
-    {% if "linux" in asset.name and "arm64" in asset.name %}
-      {% set LINUX_ARM64 = asset.url %}
-    {% endif %}
-  {% endfor %}
-
-  {% if DARWIN_AMD64 %}
+  {%- for asset in release.assets %}
+    {%- if "darwin" in asset.name and "amd64" in asset.name %}
   if OS.mac? && Hardware::CPU.intel?
-    url "{{ DARWIN_AMD64 }}"
-    sha256 "{{ DARWIN_AMD64 | sha256 }}"
+    url "{{ asset.url }}"
+    sha256 "{{ asset.url | sha256 }}"
   end
-  {% endif %}
-
-  {% if DARWIN_ARM64 %}
+    {%- endif %}
+    {%- if "darwin" in asset.name and "arm64" in asset.name %}
   if OS.mac? && Hardware::CPU.arm?
-    url "{{ DARWIN_ARM64 }}"
-    sha256 "{{ DARWIN_ARM64 | sha256 }}"
+    url "{{ asset.url }}"
+    sha256 "{{ asset.url | sha256 }}"
   end
-  {% endif %}
-
-  {% if LINUX_AMD64 %}
+    {%- endif %}
+    {%- if "linux" in asset.name and "amd64" in asset.name %}
   if OS.linux? && Hardware::CPU.intel?
-    url "{{ LINUX_AMD64 }}"
-    sha256 "{{ LINUX_AMD64 | sha256 }}"
+    url "{{ asset.url }}"
+    sha256 "{{ asset.url | sha256 }}"
   end
-  {% endif %}
-
-  {% if LINUX_ARM64 %}
+    {%- endif %}
+    {%- if "linux" in asset.name and "arm64" in asset.name %}
   if OS.linux? && Hardware::CPU.arm?
-    url "{{ LINUX_ARM64 }}"
-    sha256 "{{ LINUX_ARM64 | sha256 }}"
+    url "{{ asset.url }}"
+    sha256 "{{ asset.url | sha256 }}"
   end
-  {% endif %}
+    {%- endif %}
+  {%- endfor %}
 
   option "without-completions", "Disable bash completions"
   depends_on "bash-completion@2" => :optional
