cd234bbcc3cdb094fb0f3e6744c0eef0769d4a40
Haruaki Tamada <tamada@users.noreply.github.com>
2026-01-25 14:28:01 +0900

    new release totebag-0.8.11

--- Formula/totebag.rb
+++ Formula/totebag.rb
@@ -1,4 +1,4 @@
-VERSION="0.8.9"
+VERSION="0.8.11"
 
 class Totebag < Formula
   desc "A tool for archiving files and directories and extracting several archive formats."
@@ -9,20 +9,20 @@ class Totebag < Formula
   version VERSION
   license "MIT license"
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tamada/totebag/releases/download/v0.8.9/totebag-0.8.9_darwin_amd64.tar.gz"
-    sha256 "b35df468779d936f3c3f2c088dd2612408b7c359ba70eeb608fe3eefea80a1bc"
+    url "https://github.com/tamada/totebag/releases/download/v0.8.11/totebag-0.8.11_darwin_amd64.tar.gz"
+    sha256 "5f3e31ec9611cb80670aad265f63de4206f4e663da16ec990c8c62e2aa300b0b"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tamada/totebag/releases/download/v0.8.9/totebag-0.8.9_darwin_arm64.tar.gz"
-    sha256 "fb39cff9cee15eb83357994853bff8d26a967219276b3c76bcf33fe22ac9f025"
+    url "https://github.com/tamada/totebag/releases/download/v0.8.11/totebag-0.8.11_darwin_arm64.tar.gz"
+    sha256 "4acb6461833c7107a4facb14f11d8123e5b268c01b502e6cbe5ad66ba9c2c3bc"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tamada/totebag/releases/download/v0.8.9/totebag-0.8.9_linux_amd64.tar.gz"
-    sha256 "2deb2d7d0874fa34710ed016455c2a06381efcd649580c3008a2186d9ef9fbc2"
+    url "https://github.com/tamada/totebag/releases/download/v0.8.11/totebag-0.8.11_linux_amd64.tar.gz"
+    sha256 "9b10b5534f72f0447fc31d206f511cd5e7492893fb7d3893a211a71f8d65848e"
   end
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/tamada/totebag/releases/download/v0.8.9/totebag-0.8.9_linux_arm64.tar.gz"
-    sha256 "062b353c2bfd3469a65a53152c5882839ef281c2deaabf7542206fa45bb67d6f"
+    url "https://github.com/tamada/totebag/releases/download/v0.8.11/totebag-0.8.11_linux_arm64.tar.gz"
+    sha256 "d5ce3c21167fb88419dd2db4c5d04e3a4e5218e3badf4ceb6f57aaa4be66325d"
   end
 
   def install
