956b92384fbeab3f857e487a58f6b683a6bda4c8
Haruaki Tamada <tamada@users.noreply.github.com>
2026-02-20 13:44:43 +0900

    Update README and project data: add spellout, archive lioss, omelette, rrh, scv, tjdoe, ttt, and urleap

---
+++ .template/spellout_template.rb
@@ -0,0 +1,42 @@
+VERSION="{{ release.tagName | to_version }}"
+
+class Spellout < Formula
+  desc "A phonetic code encoder/decoder written in Rust"
+  option "without-completions", "Disable bash completions"
+  depends_on "bash-completion@2" => :optional
+
+  homepage "https://github.com/{{ project.owner }}/{{ project.name }}"
+  version VERSION
+  license "{{ project.license }}"
+
+  {%- for asset in release.assets %}
+    {%- if "darwin" in asset.name and "amd64" in asset.name %}
+  if OS.mac? && Hardware::CPU.intel?
+    {%- endif %}
+    {%- if "darwin" in asset.name and "arm64" in asset.name %}
+  if OS.mac? && Hardware::CPU.arm?
+    {%- endif %}
+    {%- if "linux" in asset.name and "amd64" in asset.name %}
+  if OS.linux? && Hardware::CPU.intel?
+    {%- endif %}
+    {%- if "linux" in asset.name and "arm64" in asset.name %}
+  if OS.linux? && Hardware::CPU.arm?
+    {%- endif %}
+    url "{{ asset.url }}"
+    sha256 "{{ asset.url | sha256 }}"
+  end
+  {%- endfor %}
+
+  def install
+    bin.install "spellout"
+    system "ln -s \"#{bin}/spellout\" #{bin}/spellout"
+
+    bash_completion.install "assets/completions/bash/spellout" if build.with? "completions"
+    zsh_completion.install  "assets/completions/zsh/_spellout" if build.with? "completions"
+    fish_completion.install "assets/completions/fish/spellout" if build.with? "completions"
+  end
+
+  test do
+    system "#{bin}/spellout --version"
+  end
+end
---
+++ Formula/spellout.rb
@@ -0,0 +1,40 @@
+VERSION="0.1.1"
+
+class Spellout < Formula
+  desc "A phonetic code encoder/decoder written in Rust"
+  option "without-completions", "Disable bash completions"
+  depends_on "bash-completion@2" => :optional
+
+  homepage "https://github.com/tamada/spellout"
+  version VERSION
+  license "MIT license"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/tamada/spellout/releases/download/v0.1.1/spellout-0.1.1_darwin_amd64.tar.gz"
+    sha256 "a72282bc32f71577d25b00c94fad0953fa86a48103aefdab51498368da04ff9f"
+  end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/tamada/spellout/releases/download/v0.1.1/spellout-0.1.1_darwin_arm64.tar.gz"
+    sha256 "bc638ba22934eba67dbb50765c5422bc79c36d848b7edf65dbd879979d0b1f87"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/tamada/spellout/releases/download/v0.1.1/spellout-0.1.1_linux_amd64.tar.gz"
+    sha256 "b76d7f27a9a764ecdc6ad4c72b384c831c6718346a6f46b285515891d3aa7028"
+  end
+  if OS.linux? && Hardware::CPU.arm?
+    url "https://github.com/tamada/spellout/releases/download/v0.1.1/spellout-0.1.1_linux_arm64.tar.gz"
+    sha256 "be1c44de5af518b0e5d95bb51663a92ea3a37dc069d70343e233f6366722b0c9"
+  end
+
+  def install
+    bin.install "spellout"
+    system "ln -s \"#{bin}/spellout\" #{bin}/spellout"
+
+    bash_completion.install "assets/completions/bash/spellout" if build.with? "completions"
+    zsh_completion.install  "assets/completions/zsh/_spellout" if build.with? "completions"
+    fish_completion.install "assets/completions/fish/spellout" if build.with? "completions"
+  end
+
+  test do
+    system "#{bin}/spellout --version"
+  end
+end
